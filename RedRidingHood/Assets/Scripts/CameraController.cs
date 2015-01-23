3dea45d30a27f8b7a209088ba568292a5988ac15
wookster77 <lochana_nerd@hotmail.com>
2015-01-24 12:07:36 +1300

    Commit of US 2 and 9
    ISSUE: Having a character, character moving right, camera follow, physics

---
+++ RedRidingHood/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    Transform startMarker;
+    public Transform playerPosition;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    public float smooth = 5.0F;
+    public float verticleOffset = 10.0f;
+    public float horizontalOffset;
+    void Start()
+    {
+        startMarker = Camera.main.transform;
+        startTime = Time.time;
+        journeyLength = Vector2.Distance(startMarker.position, playerPosition.position);
+    }
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        Vector3 position = transform.position;
+        Vector3 offsetVector = new Vector3(horizontalOffset, verticleOffset, 0);
+        position.x = Vector2.Lerp(startMarker.position, (playerPosition.position + offsetVector), fracJourney).x;
+        position.y = Vector2.Lerp(startMarker.position, (playerPosition.position + offsetVector), fracJourney).y;
+        transform.position = position;
+    }
+}
---
+++ RedRidingHood/Assets/Scripts/CharacterController2DRed.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController2DRed : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    public float horizontalForce = 10.0f;
+    string redStateAnimatorParam = "CHARACTER_STATE";
+
+    public RedRidingHoodState redRidingHoodState;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        //redRidingHoodState = RedRidingHoodState.WALKING;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveRed(redRidingHoodState);
+        AnimateMatilda(redRidingHoodState);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collission)
+    {
+
+    }
+
+
+    void OnCollisionStay2D(Collision2D collission)
+    {
+
+    }
+
+    void OnCollisionExit2D(Collision2D collission)
+    {
+
+    }
+
+
+
+    void MoveRed(RedRidingHoodState redRidingHoodState)
+    {
+        switch (redRidingHoodState)
+        {
+            case RedRidingHoodState.WALKING:
+                Debug.Log("Walking");
+                rigidbody2D.velocity = new Vector2(horizontalForce, rigidbody2D.velocity.y);
+                break;
+            case RedRidingHoodState.IDLE:
+                break;
+        }
+    }
+
+    /*
+     * 0 - Idle
+     * 1 - Walk
+     */
+    void AnimateMatilda(RedRidingHoodState redRidingHoodState)
+    {
+        switch (redRidingHoodState)
+        {
+            case RedRidingHoodState.WALKING:
+                animator.SetInteger(redStateAnimatorParam, 1);
+                break;
+            case RedRidingHoodState.IDLE:
+                animator.SetInteger(redStateAnimatorParam, 0);
+                break;
+        } 
+    }
+
+    public enum RedRidingHoodState
+    {
+        WALKING,
+        IDLE,
+    }
+}
