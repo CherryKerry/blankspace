05fd2cdee77d641742f6faaea5d86b17b3a841da
KeeganLiew <keeganliew@hotmail.com>
2015-01-24 09:29:46 +1300

    Added Prompt textbox Updated .gitignore

---
+++ RedRidingHood/Assets/Scripts/UIText/TextPrompt.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextPrompt : MonoBehaviour
+{
+
+		private Text promptTextbox;
+
+		public float fadeInTime;
+		public float fadeOutTime;
+
+		void Start ()
+		{
+				Initializations ();
+		}
+	
+		void Update ()
+		{
+		}
+
+		void Initializations ()
+		{
+				EventListenerInitializations ();
+				TimingInitialize ();
+				TextInitialize ();
+		}
+
+		void TimingInitialize ()
+		{
+				fadeInTime = 2.0f;
+				fadeOutTime = 1.0f;
+		}
+
+		void TextInitialize ()
+		{
+				promptTextbox = GetComponent <Text> ();
+				promptTextbox.CrossFadeAlpha (0f, 0.0f, false); //text first appear as faded out
+				promptTextbox.CrossFadeAlpha (1f, fadeInTime, false); //fade in text
+				string promptMessage = "";//retrieve message
+				SetPrompt (promptMessage);
+		}
+
+		void EventListenerInitializations ()
+		{
+		}
+
+		private string GetPrompt ()
+		{
+				return promptTextbox.text;
+		}
+
+		private void SetPrompt (string promptText)
+		{
+				promptTextbox.text = promptText;
+		}
+
+}
---
+++ RedRidingHood/Assets/Scripts/UIText/WordChoices.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WordChoices : MonoBehaviour
+{
+		private Text wordTextbox;
+	
+		public float fadeInTime;
+		public float fadeOutTime;
+
+		void Start ()
+		{
+				Initializations ();
+		}
+	
+		void Update ()
+		{
+		}
+		
+		void Initializations ()
+		{
+				TimingInitialize ();
+				TextInitialize ();
+		}
+	
+		void TimingInitialize ()
+		{
+				fadeInTime = 2.0f;
+				fadeOutTime = 1.0f;
+		}
+	
+		void TextInitialize ()
+		{
+				wordTextbox = GetComponent <Text> ();
+				wordTextbox.CrossFadeAlpha (0f, 0.0f, false); //text first appear as faded out
+		}
+
+		public string GetWordText ()
+		{
+				return wordTextbox.text;
+		}
+	
+		public void SetWordText (string promptText)
+		{
+				wordTextbox.text = promptText;
+		}
+
+}
