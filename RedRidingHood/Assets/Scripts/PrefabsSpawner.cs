1dbe2d13e263caad94cce77f8c0d7cf7e75ce844
wookster77 <lochana_nerd@hotmail.com>
2015-01-25 09:52:21 +1300

    Commiting PrefabsSpawner.cs
    ISSUE: Generation of objects

--- RedRidingHood/Assets/Scripts/PrefabsSpawner.cs
+++ RedRidingHood/Assets/Scripts/PrefabsSpawner.cs
@@ -9,7 +9,6 @@ public class PrefabsSpawner : MonoBehaviour {
     public GameObject[] blockPrefabs;
     public Dictionary<string, GameObject> blockTypes;
 
-
     void Start()
     {
         blockTypes = new Dictionary<string, GameObject>();
@@ -18,6 +17,35 @@ public class PrefabsSpawner : MonoBehaviour {
         {
             blockTypes[prefab.name] = prefab;
         }
+    }
+
+    void OnEnable ()
+    {
+        Manager.OnEvent += Manager_OnEvent;
+    }
+
+    void Manager_OnEvent(string keyWord, string word)
+    {
+        if (keyWord == Constants.HouseType.Keyword)
+        {
+            switch(word)
+            {
+                case Constants.HouseType.Cottage:
+                    GameObject gameObject = Instantiate(Resources.Load("/Prefabs/Cottage")) as GameObject;
+                    Vector3 loadPosition = camera.ViewportToWorldPoint(new Vector3(0.8f, 0.8f, 0));
+                    gameObject.transform.position = loadPosition;
+                    break;
+                case Constants.HouseType.House:
+                    break;
+                case Constants.HouseType.Mansion:
+                    break;
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        Manager.OnEvent -= Manager_OnEvent;
     }
 	
 	// Update is called once per frame
@@ -36,7 +64,12 @@ public class PrefabsSpawner : MonoBehaviour {
     {
         GameObject prefab = Instantiate(blockTypes[str], new Vector3(Random.Range(-4, 4), Random.Range(-3, 3), 0), Quaternion.identity) as GameObject;
         prefab.AddComponent<Rigidbody2D>();
-        prefab.GetComponent<Rigidbody2D>().gravityScale = 10f;
+        prefab.GetComponent<Rigidbody2D>().gravityScale = 8f;
         prefab.AddComponent<BoxCollider2D>();
+    }
+
+    public void DeSpawn(string str)
+    {
+        Destroy(gameObject);
     }
 }
