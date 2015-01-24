25848e6fea4773f0462eb1561aebeaa4915a29b8
mouwen <jeff345@gmail.com>
2015-01-24 13:59:10 +1300

    test

--- RedRidingHood/Assets/Scripts/ParallaxMovements/RendererExtensions.cs
+++ RedRidingHood/Assets/Scripts/ParallaxMovements/RendererExtensions.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using UnityEngine;
 
 public static class RendererExtensions
@@ -8,3 +9,15 @@ public static class RendererExtensions
         return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
     }
 }
+=======
+﻿using UnityEngine;
+
+public static class RendererExtensions
+{
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+}
+>>>>>>> Stashed changes
