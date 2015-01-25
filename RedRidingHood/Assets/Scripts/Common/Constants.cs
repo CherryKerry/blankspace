fdbd29ed863b8ccec948ee46174d75bdd49e01a0
KeeganLiew <keeganliew@hotmail.com>
2015-01-25 15:06:15 +1300

    Prefab missing fix? Constants updated

--- RedRidingHood/Assets/Scripts/Common/Constants.cs
+++ RedRidingHood/Assets/Scripts/Common/Constants.cs
@@ -2,42 +2,83 @@
 using UnityEngine;
 using System.Collections;
 
-public static class Constants {
+public static class Constants
+{
 
-    public class HoodColour
-    {
-        public const string Keyword = "[hood_colour]";
-        public const string Red = "Red";
-        public const string Blue = "Blue";
-        public const string Green = "Green";
-        public const string HotPink = "Pink";
-    }
+		public class HoodColour
+		{
+				public const string Keyword = "[hood_colour]";
+				public const string Red = "Red";
+				public const string Blue = "Blue";
+				public const string Green = "Green";
+				public const string HotPink = "Pink";
+		}
 
-    public class HouseType
-    {
-        public const string Keyword = "[house_type]";
-        public const string Cottage = "cottage";
-        public const string House = "house";
-        public const string Mansion = "mansion";
-    }
+		public class HouseType
+		{
+				public const string Keyword = "[house_type]";
+				public const string Cottage = "cottage";
+				public const string House = "house";
+				public const string Mansion = "mansion";
+		}
 
-    public class AnimalType
-    {
-        public const string Keyword = "[animal]";
-        public const string Bear = "Bear";
-        public const string Wolf = "Wolf";
-        public const string Taniwha = "Taniwha";
-    }
+		public class AnimalType
+		{
+				public const string Keyword = "[animal]";
+				public const string Bear = "Bear";
+				public const string Wolf = "Wolf";
+				public const string Taniwha = "Taniwha";
+		}
 
-    public class BlankType
-    {
-        public const string Keyword = "[blank]";
-        public const string Kitchen = "kitchen";
-        public const string Bed = "bed";
-        public const string Clothes = "clothes";
+		public class BlankType
+		{
+				public const string Keyword = "[blank]";
+				public const string Kitchen = "kitchen";
+				public const string Bed = "bed";
+				public const string Clothes = "clothes";
 
-        public const string Hills = "hills";
-        public const string Walls = "walls";
-        public const string Trees = "trees";
-    }
+				public const string Hills = "hills";
+				public const string Walls = "walls";
+				public const string Trees = "trees";
+
+				public const string Grabbed = "grabbed";
+				public const string Touched = "touched";
+				public const string Opened = "opened";
+
+				public const string Great = "great";
+				public const string Bad = "bad";
+				public const string Fun = "fun";
+		}
+	
+		public class VisitType
+		{
+				public const string Keyword = "[to_visit]";
+				public const string Aunt = "Aunt";
+				public const string Grandmother = "Grandmother";
+				public const string Cousin = "Cousin";
+		}
+	
+		public class WeaponType
+		{
+				public const string Keyword = "[weapon]";
+				public const string Kiwifruits = "kiwifruits";
+				public const string Pineapple_Lumps = "pineapple lumps";
+				public const string Pavlovas = "pavlovas";
+		}
+	
+		public class BodyPart
+		{
+				public const string Keyword = "[body_part]";
+				public const string Hands = "hands";
+				public const string Teeth = "teeth";
+				public const string Mouth = "mouth";
+		}
+	
+		public class Action
+		{
+				public const string Keyword = "[action]";
+				public const string Catch = "catch";
+				public const string Bite = "bite";
+				public const string Eat = "eat";
+		}
 }
